88edce52d1b8999fb860e7feecea9467d564d45c
Tom Bamford <tom@bamford.io>
2021-06-15 12:12:59 +0100

    Migrate Groups to Microsoft Graph
    azuread_group
    - Support updating groups
    - Add support for creating mail enabled groups (365 only due to API limitation)
    - Add support for creating Microsoft 365 groups
    - Breaking: now require at least one of security_enabled or mail_enabled to be specified

--- internal/services/groups/group_data_source.go
+++ internal/services/groups/group_data_source.go
@@ -2,11 +2,17 @@ package groups
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"net/http"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/manicminer/hamilton/msgraph"
 
 	"github.com/hashicorp/terraform-provider-azuread/internal/clients"
+	"github.com/hashicorp/terraform-provider-azuread/internal/tf"
+	"github.com/hashicorp/terraform-provider-azuread/internal/utils"
 	"github.com/hashicorp/terraform-provider-azuread/internal/validate"
 )
 
@@ -19,41 +25,26 @@ func groupDataSource() *schema.Resource {
 		},
 
 		Schema: map[string]*schema.Schema{
-			"object_id": {
-				Type:             schema.TypeString,
-				Optional:         true,
-				Computed:         true,
-				ExactlyOneOf:     []string{"display_name", "name", "object_id"},
-				ValidateDiagFunc: validate.UUID,
-			},
-
-			"description": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-
 			"display_name": {
 				Type:             schema.TypeString,
 				Optional:         true,
 				Computed:         true,
-				ExactlyOneOf:     []string{"display_name", "name", "object_id"},
+				ExactlyOneOf:     []string{"display_name", "object_id"},
 				ValidateDiagFunc: validate.NoEmptyStrings,
 			},
 
-			// TODO: remove in v2.0
-			"name": {
+			"object_id": {
 				Type:             schema.TypeString,
 				Optional:         true,
 				Computed:         true,
-				Deprecated:       "This property has been renamed to `display_name` and will be removed in version 2.0 of the AzureAD provider.",
-				ExactlyOneOf:     []string{"display_name", "name", "object_id"},
-				ValidateDiagFunc: validate.NoEmptyStrings,
+				ExactlyOneOf:     []string{"display_name", "object_id"},
+				ValidateDiagFunc: validate.UUID,
 			},
 
 			"mail_enabled": {
 				Type:     schema.TypeBool,
-				Optional: true,
 				Computed: true,
+				Optional: true,
 			},
 
 			"security_enabled": {
@@ -62,24 +53,146 @@ func groupDataSource() *schema.Resource {
 				Computed: true,
 			},
 
+			"description": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"group_types": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+
 			"members": {
 				Type:     schema.TypeList,
 				Computed: true,
-				Elem:     &schema.Schema{Type: schema.TypeString},
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
 			},
 
 			"owners": {
 				Type:     schema.TypeList,
 				Computed: true,
-				Elem:     &schema.Schema{Type: schema.TypeString},
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+
+			"types": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
 			},
 		},
 	}
 }
 
 func groupDataSourceRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	if meta.(*clients.Client).EnableMsGraphBeta {
-		return groupDataSourceReadMsGraph(ctx, d, meta)
+	client := meta.(*clients.Client).Groups.GroupsClient
+
+	var group msgraph.Group
+	var displayName string
+
+	if v, ok := d.GetOk("display_name"); ok {
+		displayName = v.(string)
 	}
-	return groupDataSourceReadAadGraph(ctx, d, meta)
+
+	var mailEnabled, securityEnabled *bool
+	if v, exists := d.GetOk("mail_enabled"); exists {
+		mailEnabled = utils.Bool(v.(bool))
+	}
+	if v, exists := d.GetOk("security_enabled"); exists {
+		securityEnabled = utils.Bool(v.(bool))
+	}
+
+	if displayName != "" {
+		filter := fmt.Sprintf("displayName eq '%s'", displayName)
+		if mailEnabled != nil {
+			filter = fmt.Sprintf("%s and mailEnabled eq %t", filter, *mailEnabled)
+		}
+		if securityEnabled != nil {
+			filter = fmt.Sprintf("%s and securityEnabled eq %t", filter, *securityEnabled)
+		}
+
+		groups, _, err := client.List(ctx, filter)
+		if err != nil {
+			return tf.ErrorDiagPathF(err, "display_name", "No group found matching specified filter (%s)", filter)
+		}
+
+		count := len(*groups)
+		if count > 1 {
+			return tf.ErrorDiagPathF(err, "display_name", "More than one group found matching specified filter (%s)", filter)
+		} else if count == 0 {
+			return tf.ErrorDiagPathF(err, "display_name", "No group found matching specified filter (%s)", filter)
+		}
+
+		group = (*groups)[0]
+	} else if objectId, ok := d.Get("object_id").(string); ok && objectId != "" {
+		g, status, err := client.Get(ctx, objectId)
+		if err != nil {
+			if status == http.StatusNotFound {
+				return tf.ErrorDiagPathF(nil, "object_id", "No group found with object ID: %q", objectId)
+			}
+			return tf.ErrorDiagF(err, "Retrieving group with object ID: %q", objectId)
+		}
+		if g == nil {
+			return tf.ErrorDiagPathF(nil, "object_id", "Group not found with object ID: %q", objectId)
+		}
+
+		if mailEnabled != nil && (g.MailEnabled == nil || *g.MailEnabled != *mailEnabled) {
+			var actual string
+			if g.MailEnabled == nil {
+				actual = "nil"
+			} else {
+				actual = fmt.Sprintf("%t", *g.MailEnabled)
+			}
+			return tf.ErrorDiagPathF(nil, "mail_enabled", "Group with object ID %q does not have the specified mail_enabled setting (expected: %t, actual: %s)", objectId, *mailEnabled, actual)
+		}
+
+		if securityEnabled != nil && (g.SecurityEnabled == nil || *g.SecurityEnabled != *securityEnabled) {
+			var actual string
+			if g.SecurityEnabled == nil {
+				actual = "nil"
+			} else {
+				actual = fmt.Sprintf("%t", *g.SecurityEnabled)
+			}
+			return tf.ErrorDiagPathF(nil, "security_enabled", "Group with object ID %q does not have the specified security_enabled setting (expected: %t, actual: %s)", objectId, *securityEnabled, actual)
+		}
+
+		group = *g
+	}
+
+	if group.ID == nil {
+		return tf.ErrorDiagF(errors.New("API returned group with nil object ID"), "Bad API Response")
+	}
+
+	d.SetId(*group.ID)
+
+	tf.Set(d, "description", group.Description)
+	tf.Set(d, "display_name", group.DisplayName)
+	tf.Set(d, "group_types", group.GroupTypes)
+	tf.Set(d, "mail_enabled", group.MailEnabled)
+	tf.Set(d, "object_id", group.ID)
+	tf.Set(d, "security_enabled", group.SecurityEnabled)
+	tf.Set(d, "types", group.GroupTypes)
+
+	members, _, err := client.ListMembers(ctx, d.Id())
+	if err != nil {
+		return tf.ErrorDiagF(err, "Could not retrieve group members for group with object ID: %q", d.Id())
+	}
+	tf.Set(d, "members", members)
+
+	owners, _, err := client.ListOwners(ctx, d.Id())
+	if err != nil {
+		return tf.ErrorDiagF(err, "Could not retrieve group owners for group with object ID: %q", d.Id())
+	}
+	tf.Set(d, "owners", owners)
+
+	return nil
 }
