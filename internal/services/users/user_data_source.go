7dc75991305b0ba9c452d0d8c7bcb03ec5670cc9
Tom Bamford <tom@bamford.io>
2021-06-15 12:22:31 +0100

    Migrate Users to Microsoft Graph
    - Remove deprecated fields
    - Remove Computed on all fields where it makes sense
      - Enables zeroing/removing property and block values
      - More reliably detect changes
    - Support importing users without breaking their password by making `password` optional+computed

--- internal/services/users/user_data_source.go
+++ internal/services/users/user_data_source.go
@@ -2,11 +2,16 @@ package users
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"net/http"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/manicminer/hamilton/msgraph"
 
 	"github.com/hashicorp/terraform-provider-azuread/internal/clients"
+	"github.com/hashicorp/terraform-provider-azuread/internal/tf"
 	"github.com/hashicorp/terraform-provider-azuread/internal/validate"
 )
 
@@ -19,32 +24,52 @@ func userDataSource() *schema.Resource {
 		},
 
 		Schema: map[string]*schema.Schema{
+			"mail_nickname": {
+				Type:             schema.TypeString,
+				Optional:         true,
+				ExactlyOneOf:     []string{"mail_nickname", "object_id", "user_principal_name"},
+				Computed:         true,
+				ValidateDiagFunc: validate.NoEmptyStrings,
+			},
+
 			"object_id": {
 				Type:             schema.TypeString,
 				Optional:         true,
 				Computed:         true,
+				ExactlyOneOf:     []string{"mail_nickname", "object_id", "user_principal_name"},
 				ValidateDiagFunc: validate.UUID,
-				ConflictsWith:    []string{"user_principal_name"},
 			},
 
 			"user_principal_name": {
 				Type:             schema.TypeString,
 				Optional:         true,
 				Computed:         true,
+				ExactlyOneOf:     []string{"mail_nickname", "object_id", "user_principal_name"},
 				ValidateDiagFunc: validate.NoEmptyStrings,
-				ConflictsWith:    []string{"object_id"},
-			},
-
-			"mail_nickname": {
-				Type:             schema.TypeString,
-				Optional:         true,
-				Computed:         true,
-				ValidateDiagFunc: validate.NoEmptyStrings,
-				ConflictsWith:    []string{"object_id", "user_principal_name"},
 			},
 
 			"account_enabled": {
 				Type:     schema.TypeBool,
+				Computed: true,
+			},
+
+			"city": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"company_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"country": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"department": {
+				Type:     schema.TypeString,
 				Computed: true,
 			},
 
@@ -54,15 +79,13 @@ func userDataSource() *schema.Resource {
 			},
 
 			"given_name": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "The given name (first name) of the user.",
+				Type:     schema.TypeString,
+				Computed: true,
 			},
 
-			"surname": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "The user's surname (family name or last name).",
+			"job_title": {
+				Type:     schema.TypeString,
+				Computed: true,
 			},
 
 			"mail": {
@@ -70,11 +93,14 @@ func userDataSource() *schema.Resource {
 				Computed: true,
 			},
 
-			// TODO: v2.0 remove this
-			"immutable_id": {
-				Type:       schema.TypeString,
-				Computed:   true,
-				Deprecated: "This property has been renamed to `onpremises_immutable_id` and will be removed in version 2.0 of the AzureAD provider",
+			"mobile_phone": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"office_location": {
+				Type:     schema.TypeString,
+				Computed: true,
 			},
 
 			"onpremises_immutable_id": {
@@ -92,101 +118,121 @@ func userDataSource() *schema.Resource {
 				Computed: true,
 			},
 
+			"postal_code": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"state": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"street_address": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			"surname": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
 			"usage_location": {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
 
-			"job_title": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "The user’s job title.",
-			},
-
-			"department": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "The name for the department in which the user works.",
-			},
-
-			"company_name": {
-				Type:     schema.TypeString,
-				Computed: true,
-				Description: "The company name which the user is associated. " +
-					"This property can be useful for describing the company that an external user comes from.",
-			},
-
-			// TODO: v2.0 remove this
-			"physical_delivery_office_name": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "The office location in the user's place of business.",
-				Deprecated:  "This property has been renamed to `office_location` and will be removed in version 2.0 of the AzureAD provider",
-			},
-
-			"office_location": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "The office location in the user's place of business.",
-			},
-
-			"street_address": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "The street address of the user's place of business.",
-			},
-
-			"city": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "The city/region in which the user is located; for example, “US” or “UK”.",
-			},
-
-			"state": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "The state or province in the user's address.",
-			},
-
-			"country": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "The country/region in which the user is located; for example, “US” or “UK”.",
-			},
-
-			"postal_code": {
-				Type:     schema.TypeString,
-				Computed: true,
-				Description: "The postal code for the user's postal address. The postal code is specific to the user's country/region. " +
-					"In the United States of America, this attribute contains the ZIP code.",
-			},
-
-			// TODO: v2.0 remove this
-			"mobile": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "The primary cellular telephone number for the user.",
-				Deprecated:  "This property has been renamed to `mobile_phone` and will be removed in version 2.0 of the AzureAD provider",
-			},
-
-			"mobile_phone": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "The primary cellular telephone number for the user.",
-			},
-
 			"user_type": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "Whether the user is homed in the current tenant or a guest user invited from another tenant.",
+				Type:     schema.TypeString,
+				Computed: true,
 			},
 		},
 	}
 }
 
 func userDataSourceRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	if meta.(*clients.Client).EnableMsGraphBeta {
-		return userDataSourceReadMsGraph(ctx, d, meta)
+	client := meta.(*clients.Client).Users.UsersClient
+
+	var user msgraph.User
+
+	if upn, ok := d.Get("user_principal_name").(string); ok && upn != "" {
+		filter := fmt.Sprintf("userPrincipalName eq '%s'", upn)
+		users, _, err := client.List(ctx, filter)
+		if err != nil {
+			return tf.ErrorDiagF(err, "Finding user with UPN: %q", upn)
+		}
+		if users == nil {
+			return tf.ErrorDiagF(errors.New("API returned nil result"), "Bad API Response")
+		}
+		count := len(*users)
+		if count > 1 {
+			return tf.ErrorDiagPathF(nil, "user_principal_name", "More than one user found with UPN: %q", upn)
+		} else if count == 0 {
+			return tf.ErrorDiagPathF(err, "user_principal_name", "User with UPN %q was not found", upn)
+		}
+		user = (*users)[0]
+	} else if objectId, ok := d.Get("object_id").(string); ok && objectId != "" {
+		u, status, err := client.Get(ctx, objectId)
+		if err != nil {
+			if status == http.StatusNotFound {
+				return tf.ErrorDiagPathF(nil, "object_id", "User not found with object ID: %q", objectId)
+			}
+			return tf.ErrorDiagF(err, "Retrieving user with object ID: %q", objectId)
+		}
+		if u == nil {
+			return tf.ErrorDiagPathF(nil, "object_id", "User not found with object ID: %q", objectId)
+		}
+		user = *u
+	} else if mailNickname, ok := d.Get("mail_nickname").(string); ok && mailNickname != "" {
+		filter := fmt.Sprintf("mailNickname eq '%s'", mailNickname)
+		users, _, err := client.List(ctx, filter)
+		if err != nil {
+			return tf.ErrorDiagF(err, "Finding user with email alias: %q", mailNickname)
+		}
+		if users == nil {
+			return tf.ErrorDiagF(errors.New("API returned nil result"), "Bad API Response")
+		}
+		count := len(*users)
+		if count > 1 {
+			return tf.ErrorDiagPathF(nil, "mail_nickname", "More than one user found with email alias: %q", upn)
+		} else if count == 0 {
+			return tf.ErrorDiagPathF(err, "mail_nickname", "User not found with email alias: %q", upn)
+		}
+		user = (*users)[0]
+	} else {
+		return tf.ErrorDiagF(nil, "One of `object_id`, `user_principal_name` or `mail_nickname` must be supplied")
 	}
-	return userDataSourceReadAadGraph(ctx, d, meta)
+
+	if user.ID == nil {
+		return tf.ErrorDiagF(errors.New("API returned user with nil object ID"), "Bad API Response")
+	}
+
+	d.SetId(*user.ID)
+
+	tf.Set(d, "account_enabled", user.AccountEnabled)
+	tf.Set(d, "city", user.City)
+	tf.Set(d, "company_name", user.CompanyName)
+	tf.Set(d, "country", user.Country)
+	tf.Set(d, "department", user.Department)
+	tf.Set(d, "display_name", user.DisplayName)
+	tf.Set(d, "given_name", user.GivenName)
+	tf.Set(d, "job_title", user.JobTitle)
+	tf.Set(d, "mail", user.Mail)
+	tf.Set(d, "mail_nickname", user.MailNickname)
+	tf.Set(d, "mobile_phone", user.MobilePhone)
+	tf.Set(d, "object_id", user.ID)
+	tf.Set(d, "office_location", user.OfficeLocation)
+	tf.Set(d, "onpremises_immutable_id", user.OnPremisesImmutableId)
+	tf.Set(d, "onpremises_sam_account_name", user.OnPremisesSamAccountName)
+	tf.Set(d, "onpremises_user_principal_name", user.OnPremisesUserPrincipalName)
+	tf.Set(d, "postal_code", user.PostalCode)
+	tf.Set(d, "state", user.State)
+	tf.Set(d, "street_address", user.StreetAddress)
+	tf.Set(d, "surname", user.Surname)
+	tf.Set(d, "usage_location", user.UsageLocation)
+	tf.Set(d, "user_principal_name", user.UserPrincipalName)
+	tf.Set(d, "user_type", user.UserType)
+
+	return nil
 }
--- internal/services/users/user_resource.go
+++ internal/services/users/user_resource.go
@@ -2,15 +2,21 @@ package users
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"log"
+	"net/http"
+	"strings"
 
 	"github.com/hashicorp/go-uuid"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/manicminer/hamilton/msgraph"
 
 	"github.com/hashicorp/terraform-provider-azuread/internal/clients"
 	"github.com/hashicorp/terraform-provider-azuread/internal/tf"
+	"github.com/hashicorp/terraform-provider-azuread/internal/utils"
 	"github.com/hashicorp/terraform-provider-azuread/internal/validate"
 )
 
@@ -20,6 +26,8 @@ func userResource() *schema.Resource {
 		ReadContext:   userResourceRead,
 		UpdateContext: userResourceUpdate,
 		DeleteContext: userResourceDelete,
+
+		CustomizeDiff: userResourceCustomizeDiff,
 
 		Importer: tf.ValidateResourceIDPriorToImport(func(id string) error {
 			if _, err := uuid.ParseUUID(id); err != nil {
@@ -42,37 +50,30 @@ func userResource() *schema.Resource {
 				ValidateDiagFunc: validate.NoEmptyStrings,
 			},
 
-			"given_name": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Computed:    true,
-				Description: "The given name (first name) of the user.",
-			},
-
-			"surname": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Computed:    true,
-				Description: "The user's surname (family name or last name).",
-			},
-
-			"mail_nickname": {
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
-			},
-
 			"account_enabled": {
 				Type:     schema.TypeBool,
 				Optional: true,
 				Default:  true,
 			},
 
-			"password": {
-				Type:         schema.TypeString,
-				Required:     true,
-				Sensitive:    true,
-				ValidateFunc: validation.StringLenBetween(1, 256), //currently the max length for AAD passwords is 256
+			"city": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+
+			"company_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+
+			"country": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+
+			"department": {
+				Type:     schema.TypeString,
+				Optional: true,
 			},
 
 			"force_password_change": {
@@ -81,27 +82,41 @@ func userResource() *schema.Resource {
 				Default:  false,
 			},
 
+			"given_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+
+			"job_title": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+
 			"mail": {
 				Type:     schema.TypeString,
+				Optional: true,
+			},
+
+			"mail_nickname": {
+				Type:     schema.TypeString,
+				Optional: true,
 				Computed: true,
 			},
 
-			// TODO: remove in v2.0
-			"immutable_id": {
-				Type:          schema.TypeString,
-				Optional:      true,
-				Computed:      true,
-				ConflictsWith: []string{"onpremises_immutable_id"},
-				Deprecated:    "This property has been renamed to `onpremises_immutable_id` and will be removed in version 2.0 of the AzureAD provider",
+			"mobile_phone": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+
+			"office_location": {
+				Type:     schema.TypeString,
+				Optional: true,
 			},
 
 			"onpremises_immutable_id": {
-				Type:          schema.TypeString,
-				Optional:      true,
-				Computed:      true,
-				ConflictsWith: []string{"immutable_id"},
-				Description: "This must be specified if you are using a federated domain for the user's userPrincipalName (UPN) property when creating a new user account. " +
-					"It is used to associate an on-premises Active Directory user account with their Azure AD user object.",
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
 			},
 
 			"onpremises_sam_account_name": {
@@ -114,110 +129,42 @@ func userResource() *schema.Resource {
 				Computed: true,
 			},
 
-			"object_id": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-
-			"usage_location": {
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
-				Description: "A two letter country code (ISO standard 3166). " +
-					"Required for users that will be assigned licenses due to legal requirement to check for availability of services in countries. " +
-					"Examples include: `NO`, `JP`, and `GB`. Not nullable.",
-			},
-
-			"job_title": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Computed:    true,
-				Description: "The user’s job title.",
-			},
-
-			"department": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Computed:    true,
-				Description: "The name for the department in which the user works.",
-			},
-
-			"company_name": {
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
-				Description: "The company name which the user is associated. " +
-					"This property can be useful for describing the company that an external user comes from.",
-			},
-
-			// TODO: remove in v2.0
-			"physical_delivery_office_name": {
-				Type:          schema.TypeString,
-				Optional:      true,
-				Computed:      true,
-				ConflictsWith: []string{"office_location"},
-				Deprecated:    "This property has been renamed to `office_location` and will be removed in version 2.0 of the AzureAD provider",
-			},
-
-			"office_location": {
-				Type:          schema.TypeString,
-				Optional:      true,
-				Computed:      true,
-				ConflictsWith: []string{"physical_delivery_office_name"},
-				Description:   "The office location in the user's place of business.",
-			},
-
-			"street_address": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Computed:    true,
-				Description: "The street address of the user's place of business.",
-			},
-
-			"city": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Computed:    true,
-				Description: "The city/region in which the user is located; for example, “US” or “UK”.",
-			},
-
-			"state": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Computed:    true,
-				Description: "The state or province in the user's address.",
-			},
-
-			"country": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Computed:    true,
-				Description: "The country/region in which the user is located; for example, “US” or “UK”.",
+			"password": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Computed:     true,
+				Sensitive:    true,
+				ValidateFunc: validation.StringLenBetween(1, 256), // Currently the max length for AAD passwords is 256
 			},
 
 			"postal_code": {
 				Type:     schema.TypeString,
 				Optional: true,
+			},
+
+			"street_address": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+
+			"state": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+
+			"surname": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+
+			"usage_location": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+
+			"object_id": {
+				Type:     schema.TypeString,
 				Computed: true,
-				Description: "The postal code for the user's postal address. The postal code is specific to the user's country/region. " +
-					"In the United States of America, this attribute contains the ZIP code.",
-			},
-
-			// TODO: remove in v2.0
-			"mobile": {
-				Type:          schema.TypeString,
-				Optional:      true,
-				Computed:      true,
-				ConflictsWith: []string{"mobile_phone"},
-				Deprecated:    "This property has been renamed to `mobile_phone` and will be removed in version 2.0 of the AzureAD provider",
-			},
-
-			"mobile_phone": {
-				Type:          schema.TypeString,
-				Optional:      true,
-				Computed:      true,
-				ConflictsWith: []string{"mobile"},
-				Description:   "The primary cellular telephone number for the user.",
 			},
 
 			"user_type": {
@@ -228,30 +175,166 @@ func userResource() *schema.Resource {
 	}
 }
 
-func userResourceCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	if meta.(*clients.Client).EnableMsGraphBeta {
-		return userResourceCreateMsGraph(ctx, d, meta)
+func userResourceCustomizeDiff(ctx context.Context, diff *schema.ResourceDiff, meta interface{}) error {
+	if diff.Id() == "" && diff.Get("password").(string) == "" {
+		return fmt.Errorf("`password` is required when creating a new user")
 	}
-	return userResourceCreateAadGraph(ctx, d, meta)
+	return nil
+}
+
+func userResourceCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*clients.Client).Users.UsersClient
+
+	upn := d.Get("user_principal_name").(string)
+	mailNickName := d.Get("mail_nickname").(string)
+
+	// Default mail nickname to the first part of the UPN (matches the portal)
+	if mailNickName == "" {
+		mailNickName = strings.Split(upn, "@")[0]
+	}
+
+	properties := msgraph.User{
+		AccountEnabled:    utils.Bool(d.Get("account_enabled").(bool)),
+		City:              utils.NullableString(d.Get("city").(string)),
+		CompanyName:       utils.NullableString(d.Get("company_name").(string)),
+		Country:           utils.NullableString(d.Get("country").(string)),
+		Department:        utils.NullableString(d.Get("department").(string)),
+		DisplayName:       utils.String(d.Get("display_name").(string)),
+		GivenName:         utils.NullableString(d.Get("given_name").(string)),
+		JobTitle:          utils.NullableString(d.Get("job_title").(string)),
+		MailNickname:      utils.String(mailNickName),
+		MobilePhone:       utils.NullableString(d.Get("mobile_phone").(string)),
+		OfficeLocation:    utils.NullableString(d.Get("office_location").(string)),
+		PostalCode:        utils.NullableString(d.Get("postal_code").(string)),
+		State:             utils.NullableString(d.Get("state").(string)),
+		StreetAddress:     utils.NullableString(d.Get("street_address").(string)),
+		Surname:           utils.NullableString(d.Get("surname").(string)),
+		UsageLocation:     utils.NullableString(d.Get("usage_location").(string)),
+		UserPrincipalName: utils.String(upn),
+
+		PasswordProfile: &msgraph.UserPasswordProfile{
+			ForceChangePasswordNextSignIn: utils.Bool(d.Get("force_password_change").(bool)),
+			Password:                      utils.String(d.Get("password").(string)),
+		},
+	}
+
+	if v, ok := d.GetOk("onpremises_immutable_id"); ok {
+		properties.OnPremisesImmutableId = utils.String(v.(string))
+	}
+
+	user, _, err := client.Create(ctx, properties)
+	if err != nil {
+		return tf.ErrorDiagF(err, "Creating user %q", upn)
+	}
+
+	if user.ID == nil || *user.ID == "" {
+		return tf.ErrorDiagF(errors.New("API returned group with nil object ID"), "Bad API Response")
+	}
+
+	d.SetId(*user.ID)
+
+	return userResourceRead(ctx, d, meta)
 }
 
 func userResourceUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	if meta.(*clients.Client).EnableMsGraphBeta {
-		return userResourceUpdateMsGraph(ctx, d, meta)
+	client := meta.(*clients.Client).Users.UsersClient
+
+	properties := msgraph.User{
+		ID:             utils.String(d.Id()),
+		AccountEnabled: utils.Bool(d.Get("account_enabled").(bool)),
+		City:           utils.NullableString(d.Get("city").(string)),
+		CompanyName:    utils.NullableString(d.Get("company_name").(string)),
+		Country:        utils.NullableString(d.Get("country").(string)),
+		Department:     utils.NullableString(d.Get("department").(string)),
+		DisplayName:    utils.String(d.Get("display_name").(string)),
+		GivenName:      utils.NullableString(d.Get("given_name").(string)),
+		JobTitle:       utils.NullableString(d.Get("job_title").(string)),
+		MailNickname:   utils.String(d.Get("mail_nickname").(string)),
+		MobilePhone:    utils.NullableString(d.Get("mobile_phone").(string)),
+		OfficeLocation: utils.NullableString(d.Get("office_location").(string)),
+		PostalCode:     utils.NullableString(d.Get("postal_code").(string)),
+		State:          utils.NullableString(d.Get("state").(string)),
+		StreetAddress:  utils.NullableString(d.Get("street_address").(string)),
+		Surname:        utils.NullableString(d.Get("surname").(string)),
+		UsageLocation:  utils.NullableString(d.Get("usage_location").(string)),
 	}
-	return userResourceUpdateAadGraph(ctx, d, meta)
+
+	if d.HasChange("password") {
+		properties.PasswordProfile = &msgraph.UserPasswordProfile{
+			ForceChangePasswordNextSignIn: utils.Bool(d.Get("force_password_change").(bool)),
+			Password:                      utils.String(d.Get("password").(string)),
+		}
+	}
+
+	if d.HasChange("onpremises_immutable_id") {
+		properties.OnPremisesImmutableId = utils.String(d.Get("onpremises_immutable_id").(string))
+	}
+
+	if _, err := client.Update(ctx, properties); err != nil {
+		return tf.ErrorDiagF(err, "Could not update user with ID: %q", d.Id())
+	}
+
+	return userResourceRead(ctx, d, meta)
 }
 
 func userResourceRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	if meta.(*clients.Client).EnableMsGraphBeta {
-		return userResourceReadMsGraph(ctx, d, meta)
+	client := meta.(*clients.Client).Users.UsersClient
+
+	objectId := d.Id()
+
+	user, status, err := client.Get(ctx, objectId)
+	if err != nil {
+		if status == http.StatusNotFound {
+			log.Printf("[DEBUG] User with Object ID %q was not found - removing from state!", objectId)
+			d.SetId("")
+			return nil
+		}
+		return tf.ErrorDiagF(err, "Retrieving user with object ID: %q", objectId)
 	}
-	return userResourceReadAadGraph(ctx, d, meta)
+
+	tf.Set(d, "account_enabled", user.AccountEnabled)
+	tf.Set(d, "city", user.City)
+	tf.Set(d, "company_name", user.CompanyName)
+	tf.Set(d, "country", user.Country)
+	tf.Set(d, "department", user.Department)
+	tf.Set(d, "display_name", user.DisplayName)
+	tf.Set(d, "given_name", user.GivenName)
+	tf.Set(d, "job_title", user.JobTitle)
+	tf.Set(d, "mail", user.Mail)
+	tf.Set(d, "mail_nickname", user.MailNickname)
+	tf.Set(d, "mobile_phone", user.MobilePhone)
+	tf.Set(d, "object_id", user.ID)
+	tf.Set(d, "office_location", user.OfficeLocation)
+	tf.Set(d, "onpremises_immutable_id", user.OnPremisesImmutableId)
+	tf.Set(d, "onpremises_sam_account_name", user.OnPremisesSamAccountName)
+	tf.Set(d, "onpremises_user_principal_name", user.OnPremisesUserPrincipalName)
+	tf.Set(d, "postal_code", user.PostalCode)
+	tf.Set(d, "state", user.State)
+	tf.Set(d, "street_address", user.StreetAddress)
+	tf.Set(d, "surname", user.Surname)
+	tf.Set(d, "usage_location", user.UsageLocation)
+	tf.Set(d, "user_principal_name", user.UserPrincipalName)
+	tf.Set(d, "user_type", user.UserType)
+
+	return nil
 }
 
 func userResourceDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	if meta.(*clients.Client).EnableMsGraphBeta {
-		return userResourceDeleteMsGraph(ctx, d, meta)
+	client := meta.(*clients.Client).Users.UsersClient
+
+	_, status, err := client.Get(ctx, d.Id())
+	if err != nil {
+		if status == http.StatusNotFound {
+			return tf.ErrorDiagPathF(fmt.Errorf("User was not found"), "id", "Retrieving user with object ID %q", d.Id())
+		}
+
+		return tf.ErrorDiagPathF(err, "id", "Retrieving user with object ID %q", d.Id())
 	}
-	return userResourceDeleteAadGraph(ctx, d, meta)
+
+	status, err = client.Delete(ctx, d.Id())
+	if err != nil {
+		return tf.ErrorDiagPathF(err, "id", "Deleting user with object ID %q, got status %d", d.Id(), status)
+	}
+
+	return nil
 }
