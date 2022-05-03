12c90314b4ebacdeea513ac685785aa7f3f4e3a7
Ashton Mickey <ashtm@microsoft.com>
2022-05-03 19:56:00 +0000

    FOR DEBUG ONLY

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"context"
+	"os"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
+	opts := &plugin.ServeOpts{
 		ProviderFunc: Provider,
-	})
+	}
+
+	if true {
+		err := plugin.Debug(context.Background(), "registry.terraform.io/hashicorp/azuread", opts)
+		if err != nil {
+			os.Exit(1)
+		}
+		return
+	}
+
+	plugin.Serve(opts)
 }
